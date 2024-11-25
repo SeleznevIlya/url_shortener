ae804a392ac681d4b362c51f993c87253ecb7b2d
Ilya Seleznev <seleznev.ilya.a@gmail.com>
2024-11-25 21:34:26 +0300

    create config

--- cmd/url_shortener/main.go
+++ cmd/url_shortener/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"url_shortener/cmd/internal/config"
+)
 
+func main() {
+	cfg := config.MustLoad()
+
+	// TODO : init logger: slog
+
+	// TODO: init storage: postgres
+
+	// TODO: init router: chi
+
+	// TODO: run server
 }
